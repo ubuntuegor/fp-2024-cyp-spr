012d2932c6ee72af576f537ad6fe0ada9fbda6a7
Ekaterina Verbitskaia <kajigor@gmail.com>
2024-02-14 20:34:08 +0200

    HW01

---
+++ Main.hs
@@ -0,0 +1,116 @@
+module Main where
+
+import Control.Monad (unless)
+import Text.Printf (printf)
+
+ageOn :: String -> Float -> Float
+ageOn planet ageInSeconds =
+  undefined
+
+isLeapYear :: Int -> Bool
+isLeapYear year =
+  undefined
+
+main = do 
+  runTests
+  putStrLn "Done"
+
+runTests = do
+    runAgeOnTests
+    runIsLeapYearTests
+  where
+    describeFailure functionName errorMsg input exp actual = 
+      printf "Test for a function %s has failed:\n  %s\n  Input: %s\n  Expected: %s\n  But got: %s\n" 
+             functionName 
+             errorMsg 
+             (show input) 
+             (show exp) 
+             (show actual)
+
+    runAgeOnTests =
+        mapM_ test cases
+      where
+        test (planet, seconds, exp) = 
+            let actual = ageOn planet seconds in 
+            unless (actual `isEqual` exp) $ describeFailure "ageOn" (printf "Wrong age on planet %s" planet :: String) seconds exp actual
+          where 
+            isEqual x y = roundTo 2 x == roundTo 2 y
+            roundTo n = (/ 10 ^ n) . fromIntegral . round . (* 10 ^ n) 
+        cases = [ ( "Earth"
+                  , 1000000000
+                  , 31.69
+                  )
+                , ( "Mercury"
+                  , 2134835688
+                  , 280.88
+                  )
+                , ( "Venus"
+                  , 189839836
+                  , 9.78
+                  )
+                , ( "Mars"
+                  , 2129871239
+                  , 35.88
+                  )
+                , ( "Jupiter"
+                  , 901876382
+                  , 2.41
+                  )
+                , ( "Saturn"
+                  , 2000000000
+                  , 2.15
+                  )
+                , ( "Uranus"
+                  , 1210123456
+                  , 0.46
+                  )
+                , ( "Neptune"
+                  , 1821023456
+                  , 0.35
+                  )
+                ]
+
+    runIsLeapYearTests =
+        mapM_ test cases
+      where
+        test (errorMsg, input, exp) =
+          let actual = isLeapYear input in 
+          unless (actual == exp) $ describeFailure "isLeapYear" errorMsg input exp actual
+         
+        cases = [ ( "year not divisible by 4 in common year"
+                  , 2015
+                  , False
+                  )
+                , ( "year divisible by 2, not divisible by 4 in common year"
+                  , 1970
+                  , False
+                  )
+                , ( "year divisible by 4, not divisible by 100 in leap year"
+                  , 1996
+                  , True
+                  )
+                , ( "year divisible by 4 and 5 is still a leap year"
+                  , 1960
+                  , True
+                  )
+                , ( "year divisible by 100, not divisible by 400 in common year"
+                  , 2100
+                  , False
+                  )
+                , ( "year divisible by 100 but not by 3 is still not a leap year"
+                  , 1900
+                  , False
+                  )
+                , ( "year divisible by 400 in leap year"
+                  , 2000
+                  , True
+                  )
+                , ( "year divisible by 400 but not by 125 is still a leap year"
+                  , 2400
+                  , True
+                  )
+                , ( "year divisible by 200, not divisible by 400 in common year"
+                  , 1800
+                  , False
+                  )
+                ]
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# fp-2024-cyp-spr
-This is a supplementary repository for the Introduction to Functional Programming course
+# HW01
+## Deadline: beginning of the class on Friday
+
+1. Given an age in seconds, calculate how old (in years) someone would be on a different planet. Implement a function `ageOn planet ageInSeconds` which returns the age in years represented as a floating point number of type `Float`. Planet name is a `String`, case sensitive. An error should be reported if an unrecognised planet is passed into. If someone tried to find their age on Pluto, explain to them that it's not a planet. 
+
+   * Mercury: orbital period 0.2408467 Earth years
+   * Venus: orbital period 0.61519726 Earth years
+   * Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
+   * Mars: orbital period 1.8808158 Earth years
+   * Jupiter: orbital period 11.862615 Earth years
+   * Saturn: orbital period 29.447498 Earth years
+   * Uranus: orbital period 84.016846 Earth years
+   * Neptune: orbital period 164.79132 Earth years
+
+2. Given a year, report if it is a leap year (function `isLeapYear year`). A leap year in the Gregorian calendar occurs:
+
+   * on every year that is evenly divisible by 4
+   * except every year that is evenly divisible by 100
+   * unless the year is also evenly divisible by 400
+
+   For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is. Report an error, if the year is negative. 
+
+## Notes 
+
+* Make a fork of this repository and checkout the branch `HW01`.
+* Write your code in the `Main.hs` file: replace `undefined` with your definitions.
+* Make sure that your tests pass, i.e. running `main` (or `./Main`) only outputs `Done`.
+* If you need more tests, add them, but don't delete the ones which are already in the file. 
+* When finished, open a pull request into the main repo. Make sure to put your name in the title of the PR.  
